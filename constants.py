d58adcfacffe23a8223c551b0a98ab524d70d2f2
Malcolm Anderson <malcolminyo@gmail.com>
2022-09-21 21:31:52 -0700

    Initial commit

---
+++ constants.py
@@ -0,0 +1,215 @@
+from textwrap import dedent
+form_url = "https://forms.hsforms.com/submissions/v3/public/submit/formsnext/multipart/5592731/7b009b60-4421-421c-9430-fbab2db126a4"
+
+education_level_acquired_options = [
+    "Current undergraduate student",
+    "Current graduate student",
+    "Former college student (did not graduate)",
+    "College graduate",
+    "Did not attend college",
+    "Other",
+]
+
+student_debt_options = [
+    "Currently have student loan debt",
+    "Did have student loan debt, but paid it off",
+    "Never took out student loans",
+]
+
+income_over__125k_options = ["Yes", "No"]
+
+received_pell_grant_options = ["Yes", "No", "Did not attend college"]
+
+frustrations_with_biden_options = [
+    "It just isn't fair! I had to work like hell for a whole thirty minutes to pay off my $32 tuition back in '53. Kids these days are too entitled!",
+    "They're just going to end up defaulting on the loans anyway, and then the taxpayers will have to foot the bill. This is socialism at its finest!",
+    "I'm all for helping out those in need, but this is just a free ride for lazy people who didn't bother to budget or plan ahead.",
+    "Biden is just catering to his base of young, liberal voters. He doesn't care about the rest of us.",
+    "What's next? forgiving mortgage debt? car loans? I bet he'll be doing that next.",
+    "This is just another example of the government getting too involved in our lives. Stay out of my pocket, Biden!",
+    "I'm so sick of these entitled kids getting everything handed to them on a silver platter. This country is going to the dogs!",
+    "I don't see why the government should be in the business of forgiving student loan debt. It's not their money to begin with!",
+    "And what about people like me who didn't go to college? Why should we have to pay for other people's mistakes?",
+    "This is just another example of the government wasting our hard-earned tax dollars.",
+    "I'm not against helping people, but there's got to be a better way to do this. This is just a band-aid solution to a much bigger problem.",
+    "I'm frustrated because I think this is a Band-Aid solution to a much bigger problem.",
+    "I'm frustrated because I think this is going to end up costing the taxpayers more money in the long run.",
+    "I'm frustrated because I think this is going to incentivize people to take on more debt than they can afford.",
+    "I'm frustrated because I think this is going to create more problems down the road.",
+    "I'm frustrated because I think this is going to do more harm than good.",
+    "Barack Obama is spying on me through my microwave oven. The Biden administration is canceling student loan debt for some people, but not for me. I'm frustrated because I don't think I should have to pay back my student loans. I'm paranoid that the government is going to confiscate my hard-earned money to pay for someone else's education.",
+    "QAnon told me that the Deep State is trying to destroy America by cancel student loan debt. They're using our money to fund their socialist agenda. I'm frustrated because I didn't go to college and I don't think I should have to pay for someone else's education.",
+    "I'm frustrated because I think the government is using student loan debt forgiveness as a way to buy votes. They're pandering to a group of people who will probably never pay back their loans anyway. I don't think it's fair that I have to keep paying my loans while other people get a free pass.",
+    "I'm frustrated because I'm a responsible person who paid off my student loans. I don't think it's fair that the government is forgiving loans for people who didn't bother to pay them back. I think it sends the wrong message.",
+    "I'm frustrated with the Biden administration cancelling student loan debt for some people because it feels like they are playing favorites. I worked hard to get my degree and I'm still paying off my loans, so why should someone who didn't have to pay anything get their debt forgiven? It's not fair.",
+    "I'm also worried that this is just a way for the government to control people. If they can forgive someone's debt, they can also force them to do things they don't want to do. This is how they'll get people to comply with their agenda. We can't let them get away with this!",
+    "Blorf! Blorf blorf blorf blorf! Blorf, blorf! BLEEP BLORP BLORF!",
+    "Blorf! Blorf, blorf! Blorf blorf blorf! BLEEP!",
+    "Blorf blorf blorf! Blorf, blorf! BLEEP BLORP BLORF!",
+    "Blorf! Blorf! Blorf, blorf! BLEEP BLORP BLORF!",
+    "Mama mia! President a-Biden is just forgiving the-a student loans? This is pizza-pasta preposterous!",
+    "Wah-ha-ha-ha! What a load of Baloney! This is just another one of Biden's tax-and-spend schemes!",
+    "He's gonna bankrupt the country! I tell-a you what, if he gets his way, we'll all be eating-a spaghetti and-a-meatballs for-a lunch!",
+    "I'm so angry about President Biden forgiving student loan debt! I'm a tiny mouse who lives in a mushroom house in the forest, and I can't believe he would do something like this! I'm just trying to survive out here, and this is going to make it so much harder for me! I can't believe he would do this to us!",
+    "Why did President Biden forgive student loan debt? Doesn't he know how hard it is for us little creatures in the forest? We have to work so hard just to survive, and this is going to make it even harder! I can't believe he would do this to us!",
+    "How could President Biden forgive student loan debt? Doesn't he know how difficult it is for us to get by in the forest? We work tirelessly just to make ends meet, and this is only going to make it harder! I can't believe he would do something like this!",
+    "President Biden forgiving student loan debt is outrageous! I'm a tiny mouse who lives in a mushroom house in the forest, and I can't believe he would do something like this! It's so unfair to us little guys who are just trying to get by!",
+    "I want money too!",
+    "I'm more than happy to forgive student loans! I think it's great!",
+    "aaaaaaaaaassdsddssddsdsdsfsdsddsdssdddddddddddddddddddddddddddddddddddddd",
+    "lkjjlmjlmkjlkmkljkkmkkkkkkkkkkkkkkkkkkkkkkjkkjjjkkkkkkkkkkkkukukkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk",
+    "'); DROP TABLE responses;",
+    "there are many",
+    "a",
+    "pretty woman hot sex sexy",
+    "MEOW",
+    "HONK",
+    "WOOF",
+    dedent(
+        """Somebody once told me the world is gonna roll me
+    I ain't the sharpest tool in the shed
+    She was looking kind of dumb with her finger and her thumb
+    In the shape of an "L" on her forehead
+    Well the years start coming and they don't stop coming
+    Fed to the rules and I hit the ground running
+    Didn't make sense not to live for fun
+    Your brain gets smart but your head gets dumb
+    So much to do, so much to see
+    So what's wrong with taking the back streets?
+    You'll never know if you don't go
+    You'll never shine if you don't glow
+    Hey now, you're an all-star, get your game on, go play
+    Hey now, you're a rock star, get the show on, get paid
+    And all that glitters is gold
+    Only shooting stars break the mold
+    It's a cool place and they say it gets colder
+    You're bundled up now, wait 'til you get older
+    But the meteor men beg to differ
+    Judging by the hole in the satellite picture
+    The ice we skate is getting pretty thin
+    The water's getting warm so you might as well swim
+    My world's on fire, how about yours?
+    That's the way I like it and I'll never get bored
+    Hey now, you're an all-star, get your game on, go play
+    Hey now, you're a rock star, get the show on, get paid
+    All that glitters is gold
+    Only shooting stars break the mold
+    Hey now, you're an all-star, get your game on, go play
+    Hey now, you're a rock star, get the show, on get paid
+    And all that glitters is gold
+    Only shooting stars
+    Somebody once asked could I spare some change for gas?
+    I need to get myself away from this place
+    I said, "Yup" what a concept
+    I could use a little fuel myself
+    And we could all use a little change
+    Well, the years start coming and they don't stop coming
+    Fed to the rules and I hit the ground running
+    Didn't make sense not to live for fun
+    Your brain gets smart but your head gets dumb
+    So much to do, so much to see
+    So what's wrong with taking the back streets?
+    You'll never know if you don't go (go!)
+    You'll never shine if you don't glow
+    Hey now, you're an all-star, get your game on, go play
+    Hey now, you're a rock star, get the show on, get paid
+    And all that glitters is gold
+    Only shooting stars break the mold
+    And all that glitters is gold
+    Only shooting stars break the mold"""
+    ),
+    dedent(
+        """1:1 In da beginning when God cweated da heavens and da earth,
+    1:2 da earth was all bwank and empty and dah was darkness everywhewe. But den God said, "Let dere be light!" and den dere was light.
+    1:3 God saw dat da light was good, so he sepawated it from da darkness.
+    1:4 God called da light "day" and he called da darkness "night." So dere was evening, and dere was morning—da first day.
+    1:5 God said, "Let dere be a space in da middwe of da waters, and let it sepawate da watahs from each oder."
+    1:6 So God made da space and sepawated da watahs above from da watahs below. And it was so.
+    1:7 God called da space "sky." So dere was evening, and dere was morning—da second day.
+    1:8 God said, "Let da watahs unduh da sky be gadered to one place, and let da dry wand appeah." And it was so.
+    1:9 God called da dry wand "Eawth," and he called da gadewing of da watahs "Seas." And God saw dat it was good.
+    1:10 God said, "Let da Eawth bwing fowth gwass and weeds and tiwes dat bwing fowth seeeds. Let dem make fwuits and veggies dat have seeeds in dem." And it was so.
+    1:11 So da Eawth bwought fowth gwass and weeds and tiwes dat bwought fowth seeeds, accoding to dere kinds. And God saw dat it was good.
+    1:12 God said, "Let da Seas bwing fowth wivving cweatuwes dat have swimmy fins, and let birds fwy above da Eawth acwoss da space in da sky."
+    1:13 So God cweated da big sea cweatuwes and all da oder cweatuwes dat live in da watahs, accoding to dere kinds. And God saw dat it was good.
+    1:14 God said, "Let us make humankind in our image, accoding to our likeness. Let dem have powah owah da fish of da sea, da birds of da air, da cweatuwes dat move along da gwound, and owah all da oder wivving cweatuwes."
+    1:15 So God cweated humankind in his own image;
+    in de image of God he cweated dem;
+    male and female he cweated dem.
+    1:16 God blessed dem and said to dem, "Be fwuitful and multiply. Fill da Eawth and subdue it. Have powah owah da fish of da sea, da birds of da air, and owah all da cweatuwes dat move along da gwound."
+    1:17 God said, "I am giving you evwything dat seeds, tiwes, and wegs. Evwything dat bwings fowth seeeds on da Eawth will be youws.
+    1:18 And I am giving you da gwass and weeds fow fwuit and veggies dat have seeeds in dem. Evwything on da Eawth that bwings fowth seeeds will be youws.
+    1:19 And I am giving you evewything with wegs fow fwuit and veggies dat have seeeds in dem. Evwything on da Eawth that bwings fowth seeeds will be youws.
+    1:20 God saw evwyting dat he had made, and indeed, it was very good. So dere was evening, and dere was morning—da sixth day.
+    1:21 Thus da heavens and da Eawth and all deir host were finished.
+    1:22 God blessed da sevend day and made it howiday, because on dat day he rested fwom all de wok dat he had done in creation."""
+    ),
+    "He can't keep getting away with this bull shit !  Did you see this video of him ?  https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    "Let's Go Branden",
+    "Lets go Brandon !",
+    "Let's Go Brandon",
+    "I hate poors",
+    "I'm going to be dead in like two years so why should I have to pay for everyone else to stay alive???",
+    "fuck joe  biden",
+    "maga",
+    "QAnon told me so !",
+]
+
+with open("beemovie.txt", "r") as _f:
+    beemovie = _f.read()
+    frustrations_with_biden_options.append(beemovie)
+
+
+state_options = [
+    "Alabama",
+    "Alaska",
+    "Arizona",
+    "Arkansas",
+    "California",
+    "Colorado",
+    "Connecticut",
+    "Delaware",
+    "District of Columbia",
+    "Florida",
+    "Georgia",
+    "Hawaii",
+    "Idaho",
+    "Illinois",
+    "Indiana",
+    "Iowa",
+    "Kansas",
+    "Kentucky",
+    "Louisiana",
+    "Maine",
+    "Maryland",
+    "Massachusetts",
+    "Michigan",
+    "Minnesota",
+    "Mississippi",
+    "Missouri",
+    "Montana",
+    "Nebraska",
+    "Nevada",
+    "New Hampshire",
+    "New Jersey",
+    "New Mexico",
+    "New York",
+    "North Carolina",
+    "North Dakota",
+    "Ohio",
+    "Oklahoma",
+    "Oregon",
+    "Pennsylvania",
+    "Rhode Island",
+    "South Carolina",
+    "South Dakota",
+    "Tennessee",
+    "Texas",
+    "Utah",
+    "Vermont",
+    "Virginia",
+    "Washington",
+    "West Virginia",
+    "Wisconsin",
+    "Wyoming",
+]
